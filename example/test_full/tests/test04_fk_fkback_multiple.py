a9f3f488f4c8232cca2a487040a6750fc539a832
Jörg Breitbart <jerch@rockborn.de>
2018-03-06 22:36:11 +0100

    first m2m tests

---
+++ example/test_full/tests/test04_fk_fkback_multiple.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from base import GenericModelTestBase, MODELS
+
+
+class MultipleDependenciesOne(GenericModelTestBase):
+    def setUp(self):
+        self.setDeps({
+            # fk + fk + fk_back + fk_back
+            'C': {'depends': ['f_cb.f_ba.ag_f.gd_f#name', 'cd_f.de_f#name'],
+                  'func': lambda self: self.name + ''.join(
+                      MODELS['D'].objects.filter(f_dg__in=MODELS['G'].objects.filter(
+                          f_ga=self.f_cb.f_ba)).values_list('name', flat=True)) + ''.join(
+                      MODELS['E'].objects.filter(f_ed__in=self.cd_f.all()).values_list('name', flat=True)
+                  )},
+        })
+        self.a = self.models.A(name='a')
+        self.a.save()
+        self.b = self.models.B(name='b', f_ba=self.a)
+        self.b.save()
+        self.c = self.models.C(name='c', f_cb=self.b)
+        self.c.save()
+        self.d = self.models.D(name='d', f_dc=self.c)
+        self.d.save()
+        self.e = self.models.E(name='e', f_ed=self.d)
+        self.e.save()
+        self.f = self.models.F(name='f', f_fe=self.e)
+        self.f.save()
+        self.g = self.models.G(name='g', f_gf=self.f, f_ga=self.a)
+        self.g.save()
+        self.d.f_dg = self.g
+        self.d.save()
+
+    def tearDown(self):
+        self.resetDeps()
+
+    def test_C_insert(self):
+        self.c.refresh_from_db()
+        self.assertEqual(self.c.comp, 'cde')
+
+    def test_C_update(self):
+        self.c.refresh_from_db()
+        self.assertEqual(self.c.comp, 'cde')
+        # change D
+        self.d.name = 'D'
+        self.d.save()
+        self.c.refresh_from_db()
+        self.assertEqual(self.c.comp, 'cDe')
+        # add new D
+        new_d = self.models.D(name='d2', f_dg=self.g)
+        new_d.save()
+        self.c.refresh_from_db()
+        self.assertEqual(self.c.comp, 'cDd2e')
+        # change E
+        self.e.name = 'E'
+        self.e.save()
+        self.c.refresh_from_db()
+        self.assertEqual(self.c.comp, 'cDd2E')
+        # add new E
+        new_e = self.models.E(name="e2", f_ed=self.d)
+        new_e.save()
+        self.c.refresh_from_db()
+        self.assertEqual(self.c.comp, 'cDd2Ee2')
+
+
+class MultipleDependenciesTwo(GenericModelTestBase):
+    def setUp(self):
+        self.setDeps({
+            # fk_back + fk_back + fk_back + fk + fk + fk
+            'D': {'depends': ['de_f.ef_f.fg_f.f_ga.f_ac.f_cb#name', 'f_dc.f_cb#name'],
+                  'func': lambda self: self.name + ''.join(filter(bool, MODELS['G'].objects.filter(
+                      f_gf__in=MODELS['F'].objects.filter(
+                          f_fe__in=self.de_f.all())).values_list(
+                      'f_ga__f_ac__f_cb__name', flat=True))) + self.f_dc.f_cb.name}
+        })
+        self.a = self.models.A(name='a')
+        self.a.save()
+        self.b = self.models.B(name='b', f_ba=self.a)
+        self.b.save()
+        self.c = self.models.C(name='c', f_cb=self.b)
+        self.c.save()
+        self.a.f_ac = self.c
+        self.a.save()
+        self.d = self.models.D(name='d', f_dc=self.c)
+        self.d.save()
+        self.e = self.models.E(name='e', f_ed=self.d)
+        self.e.save()
+        self.f = self.models.F(name='f', f_fe=self.e)
+        self.f.save()
+        self.g = self.models.G(name='g', f_gf=self.f, f_ga=self.a)
+        self.g.save()
+
+    def tearDown(self):
+        self.resetDeps()
+
+    def test_D_insert(self):
+        self.d.refresh_from_db()
+        self.assertEqual(self.d.comp, 'dbb')
+
+    def test_D_update(self):
+        self.d.refresh_from_db()
+        self.assertEqual(self.d.comp, 'dbb')
+        # change B --> should change both deps
+        self.b.name = 'B'
+        self.b.save()
+        self.d.refresh_from_db()
+        self.assertEqual(self.d.comp, 'dBB')
+        # add new A, B and C, change f_ga
+        new_b = self.models.B(name='b2')
+        new_b.save()
+        new_c = self.models.C(name='c2', f_cb=new_b)
+        new_c.save()
+        new_a = self.models.A(name='A', f_ac=new_c)
+        new_a.save()
+        self.g.f_ga = new_a
+        self.g.save()
+        self.d.refresh_from_db()
+        # this should only change the "first" B dep
+        self.assertEqual(self.d.comp, 'db2B')
---
+++ example/test_full/tests/test05_m2m.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from base import GenericModelTestBase, MODELS
+
+
+def h(f):
+    # TODO: note for doc: computed fields depending on own m2m fields need initial state
+    def wrap(self):
+        if not self.pk:
+            return ''
+        return f(self)
+    return wrap
+
+
+class M2MDependencies(GenericModelTestBase):
+    def setUp(self):
+        self.setDeps({
+            'A': {'func': lambda self: self.name},
+            'B': {'depends': ['m_ba#name'],
+                  'func': h(lambda self: self.name + ''.join(
+                      self.m_ba.all().values_list('name', flat=True)))},
+            'C': {'depends': ['m_cb.m_ba#name'],
+                  'func': h(lambda self: self.name + self.m_cb.first().m_ba.last().name)}
+        })
+        self.a1 = self.models.A(name='a1')
+        self.a1.save()
+        self.a2 = self.models.A(name='a2')
+        self.a2.save()
+        self.a3 = self.models.A(name='a3')
+        self.a3.save()
+        self.b1 = self.models.B(name='b1')
+        self.b1.save()
+        self.b1.m_ba.add(self.a1, self.a2, self.a3)
+        self.b2 = self.models.B(name='b2')
+        self.b2.save()
+        self.b2.m_ba.add(self.a1, self.a2, self.a3)
+        self.b3 = self.models.B(name='b3')
+        self.b3.save()
+        self.b3.m_ba.add(self.a1, self.a2, self.a3)
+        self.c1 = self.models.C(name='c1')
+        self.c1.save()
+        self.c1.m_cb.add(self.b1, self.b2, self.b3)
+        self.c2 = self.models.C(name='c2')
+        self.c2.save()
+        self.c2.m_cb.add(self.b1, self.b2, self.b3)
+        self.c3 = self.models.C(name='c3')
+        self.c3.save()
+        self.c3.m_cb.add(self.b1, self.b2, self.b3)
+
+    def tearDown(self):
+        self.resetDeps()
+
+    def test_insert(self):
+        self.b1.refresh_from_db()
+        self.b2.refresh_from_db()
+        self.b3.refresh_from_db()
+        self.assertEqual(self.b1.comp, 'b1a1a2a3')
+        self.assertEqual(self.b2.comp, 'b2a1a2a3')
+        self.assertEqual(self.b3.comp, 'b3a1a2a3')
+        self.c1.refresh_from_db()
+        self.c2.refresh_from_db()
+        self.c3.refresh_from_db()
+        self.assertEqual(self.c1.comp, 'c1a3')
+        self.assertEqual(self.c2.comp, 'c2a3')
+        self.assertEqual(self.c3.comp, 'c3a3')
+
+    def test_update(self):
+        self.a1.name = 'A1'
+        self.a1.save()
+        self.b1.refresh_from_db()
+        self.b2.refresh_from_db()
+        self.b3.refresh_from_db()
+        self.assertEqual(self.b1.comp, 'b1A1a2a3')
+        self.assertEqual(self.b2.comp, 'b2A1a2a3')
+        self.assertEqual(self.b3.comp, 'b3A1a2a3')
+        self.a3.name = 'A3'
+        self.a3.save()
+        self.c1.refresh_from_db()
+        self.c2.refresh_from_db()
+        self.c3.refresh_from_db()
+        self.assertEqual(self.c1.comp, 'c1A3')
+        self.assertEqual(self.c2.comp, 'c2A3')
+        self.assertEqual(self.c3.comp, 'c3A3')
+        new_a = self.models.A(name='a4')
+        new_a.save()
+        self.b1.m_ba.add(new_a)
+        self.c1.refresh_from_db()
+        self.c2.refresh_from_db()
+        self.c3.refresh_from_db()
+        self.assertEqual(self.c1.comp, 'c1a4')
+        self.assertEqual(self.c2.comp, 'c2a4')
+        self.assertEqual(self.c3.comp, 'c3a4')
