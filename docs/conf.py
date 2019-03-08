c45f19b310d0495d5428b4fcf78e9b1d7f21e2ef
Jörg Breitbart <jerch@rockborn.de>
2019-03-08 22:55:05 +0100

    skip version number due to strict pypi rules

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'netzkolchose'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.9'
+release = '0.0.10'
 
 
 # -- General configuration ---------------------------------------------------
