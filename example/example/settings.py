0d77ddf3bd9021a6d62e2389eb6d44c78111ec3e
Jörg Breitbart <jerch@rockborn.de>
2020-04-19 22:47:51 +0200

    remove unused django_extensions

--- example/example/settings.py
+++ example/example/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
+    'django.contrib.staticfiles'
 ]
 
 COMPUTEDFIELDS_ADMIN = True
