13b19460fac6066d244eb7f490a7c998549f0802
Jörg Breitbart <jerch@rockborn.de>
2020-05-19 13:01:36 +0200

    add mro and modelgraphs to admin view, change to svg output

--- computedfields/admin.py
+++ computedfields/admin.py
@@ -7,6 +7,7 @@ from django.utils.html import escape, mark_safe, format_html
 from django.urls import reverse, NoReverseMatch
 from django.conf.urls import url
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 try:
     import pygments
     from pygments.lexers import PythonLexer, JsonLexer
@@ -27,7 +28,7 @@ class ComputedModelsAdmin(admin.ModelAdmin):
     """
     actions = None
     change_list_template = 'computedfields/change_list.html'
-    list_display = ('name', 'dependencies')
+    list_display = ('name', 'computed_fields', 'dependencies', 'local_computed_fields_mro', 'modelgraph')
     list_display_links = None
 
     def has_add_permission(self, request, obj=None):
@@ -45,6 +46,32 @@ class ComputedModelsAdmin(admin.ModelAdmin):
                     pygments.highlight(s, JsonLexer(stripnl=False),
                         HtmlFormatter(noclasses=True, nowrap=True)))
         return format_html(u'<pre>{}</pre>', s)
+    
+    def computed_fields(self, inst):
+        model = apps.get_model(inst.app_label, inst.model)
+        cfs = list(model._computed_fields.keys())
+        s = dumps(cfs, indent=4, sort_keys=True)
+        if pygments:
+            s = mark_safe(
+                    pygments.highlight(s, JsonLexer(stripnl=False),
+                        HtmlFormatter(noclasses=True, nowrap=True)))
+        return format_html(u'<pre>{}</pre>', s)
+    
+    def local_computed_fields_mro(self, inst):
+        model = apps.get_model(inst.app_label, inst.model)
+        cfs = model._computed_fields.keys()
+        entry = ComputedFieldsModelType._local_mro.get(model, {'base': [], 'fields': {}})
+        base = entry['base']
+        deps = {'nonlocal': [], 'mro': base, 'fields': {}}
+        deps['nonlocal'] = list(set(cfs) - set(base))
+        for field, value in entry['fields'].items():
+            deps['fields'][field] = [name for pos, name in enumerate(base) if value & (1 << pos)]
+        s = dumps(deps, indent=4, sort_keys=False)
+        if pygments:
+            s = mark_safe(
+                pygments.highlight(s, JsonLexer(stripnl=False), HtmlFormatter(noclasses=True, nowrap=True)))
+        return format_html(u'<pre>{}</pre>', s)
+        #return 'has local deps (TODO)'
 
     def name(self, obj):
         name = escape(u'%s.%s' % (obj.app_label, obj.model))
@@ -61,8 +88,23 @@ class ComputedModelsAdmin(admin.ModelAdmin):
             url('^computedfields/rendergraph/$',
                 self.admin_site.admin_view(self.render_graph),
                 name='%s_%s_computedfields_rendergraph' % info),
+            url('^computedfields/renderuniongraph/$',
+                self.admin_site.admin_view(self.render_uniongraph),
+                name='%s_%s_computedfields_renderuniongraph' % info),
+            url('^computedfields/modelgraph/(?P<modelid>\d+)/$',
+                self.admin_site.admin_view(self.render_modelgraph),
+                name='%s_%s_computedfields_modelgraph' % info),
         ]
         return databaseview_urls + urls
+
+    def modelgraph(self, inst):
+        model = apps.get_model(inst.app_label, inst.model)
+        if not ComputedFieldsModelType._local_mro.get(model, None):
+            return 'None'
+        url = reverse('admin:%s_%s_computedfields_modelgraph' %
+                (self.model._meta.app_label,  self.model._meta.model_name),  args=[inst.id])
+        return  mark_safe('''<a href="%s" target="popup"
+                   onclick="javascript:open('', 'popup', 'height=400,width=600,resizable=yes')">ModelGraph</a>''' % url)
 
     def render_graph(self, request, extra_context=None):
         error = 'graphviz must be installed to use this feature.'
@@ -76,6 +118,46 @@ class ComputedModelsAdmin(admin.ModelAdmin):
                 graph = ComputedModelsGraph(ComputedFieldsModelType._computed_models)
                 graph.remove_redundant()
             dot = mark_safe(str(graph.get_dot()).replace('\n', ' '))
+        return render(request, 'computedfields/graph.html', {'error': error, 'dot': dot})
+    
+    def render_uniongraph(self, request, extra_context=None):
+        error = 'graphviz must be installed to use this feature.'
+        dot = ''
+        if Digraph:
+            error = ''
+            graph = ComputedFieldsModelType._graph
+            if not graph:
+                # we are in map file mode - create new graph
+                from computedfields.graph import ComputedModelsGraph
+                graph = ComputedModelsGraph(ComputedFieldsModelType._computed_models)
+                graph.remove_redundant()
+            uniongraph = graph.get_uniongraph()
+            dot = mark_safe(str(uniongraph.get_dot()).replace('\n', ' '))
+        return render(request, 'computedfields/graph.html', {'error': error, 'dot': dot})
+    
+    def render_modelgraph(self, request, modelid, extra_context=None):
+        try:
+            inst = self.model.objects.get(pk=modelid)
+            model = apps.get_model(inst.app_label, inst.model)
+        except ObjectDoesNotExist:
+            error = 'illegal value for model'
+            return render(request, 'computedfields/graph.html', {'error': error, 'dot': ''})
+        error = 'graphviz must be installed to use this feature.'
+        dot = ''
+        if Digraph:
+            error = ''
+            graph = ComputedFieldsModelType._graph
+            if not graph:
+                # we are in map file mode - create new graph
+                from computedfields.graph import ComputedModelsGraph
+                graph = ComputedModelsGraph(ComputedFieldsModelType._computed_models)
+                graph.remove_redundant()
+                graph.get_uniongraph()
+            modelgraph = graph.modelgraphs.get(model, None)
+            if modelgraph:
+                dot = mark_safe(str(modelgraph.get_dot()).replace('\n', ' '))
+            else:
+                error = 'Model has no local field dependencies.'
         return render(request, 'computedfields/graph.html', {'error': error, 'dot': dot})
 
 
