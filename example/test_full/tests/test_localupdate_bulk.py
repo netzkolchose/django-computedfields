80b1a8a59c87cd7ccf1a96d211c1053d4fce3cb4
Jörg Breitbart <jerch@rockborn.de>
2020-05-27 21:20:11 +0200

    update_dependent_multi test with local updates

--- example/test_full/tests/test_localupdate_bulk.py
+++ example/test_full/tests/test_localupdate_bulk.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from ..models import ComputeLocal, LocalBulkUpdate
-from computedfields.models import update_dependent
+from computedfields.models import update_dependent, update_dependent_multi
 
 class UpdateDependentWithLocals(TestCase):
     def setUp(self):
@@ -13,6 +13,21 @@ class UpdateDependentWithLocals(TestCase):
     def test_update(self):
         ComputeLocal.objects.all().update(name='other')
         update_dependent(ComputeLocal.objects.all(), update_fields=['name'])
+        self.cl.refresh_from_db()
+        self.bu.refresh_from_db()
+        self.assertEqual(self.cl.c1, 'OTHER')
+        self.assertEqual(self.cl.c2, 'c2OTHER')
+        self.assertEqual(self.cl.c3, 'c3OTHER')
+        self.assertEqual(self.cl.c4, 'c4c3OTHER')
+        self.assertEqual(self.cl.c5, 'c5c2OTHERc4c3OTHERc6123')
+        self.assertEqual(self.cl.c6, 'c6123')
+        self.assertEqual(self.cl.c7, 'c7c8')
+        self.assertEqual(self.cl.c8, 'c8')
+        self.assertEqual(self.bu.same_as_fk_c5, 'c5c2OTHERc4c3OTHERc6123')
+
+    def test_update_multi(self):
+        ComputeLocal.objects.all().update(name='other')
+        update_dependent_multi([ComputeLocal.objects.all()])
         self.cl.refresh_from_db()
         self.bu.refresh_from_db()
         self.assertEqual(self.cl.c1, 'OTHER')
