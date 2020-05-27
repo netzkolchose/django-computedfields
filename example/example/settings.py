373a19fa2aafe37a0c211ed82ffc3cf24479ccf1
Jörg Breitbart <jerch@rockborn.de>
2020-05-27 20:19:47 +0200

    remove benchmark app

--- example/example/settings.py
+++ example/example/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'computedfields',
     'test_full',
     'exampleapp',
-    'benchmark',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
