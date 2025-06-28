a2ed67ab946142467e55ba223c5781d1c410f0ea
jerch <jerch@rockborn.de>
2025-06-28 10:49:16 +0200

    Update test_computed_foreign_keys.py
    add testcase for cascade deletion

--- example/test_full/tests/test_computed_foreign_keys.py
+++ example/test_full/tests/test_computed_foreign_keys.py
@@ -19,3 +19,20 @@ class ComputedForeignKeys(TestCase):
                     v = models.CFKRelatedData.objects.create(parent=m, value=value)
                     self.assertEqual(v.c1, c1)
                     self.assertEqual(v.c2, c2)
+
+    def test_cascade_delete(self):
+        for c1 in models.CFKCatalogue1.objects.all():
+            for c2 in models.CFKCatalogue2.objects.all():
+                m = models.CFKData.objects.create(c1name=c1.name, c2name=c2.name)
+                for value in self.names:
+                    models.CFKRelatedData.objects.create(parent=m, value=value)
+        self.assertGreater(models.CFKData.objects.all().count(), 0)
+        self.assertGreater(models.CFKRelatedData.objects.all().count(), 0)
+
+        # on deleting all CFKCatalogue1 instances
+        # CFKData and CFKRelatedData should drop to zero count
+        # due to cascade deletion
+        for c1 in models.CFKCatalogue1.objects.all():
+            c1.delete()
+        self.assertEqual(models.CFKData.objects.all().count(), 0)
+        self.assertEqual(models.CFKRelatedData.objects.all().count(), 0)
