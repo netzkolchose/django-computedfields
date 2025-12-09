52695a63b94e5eb2e533927bae5fd3a5e167dc6b
ckarli <87313837+ckarli@users.noreply.github.com>
2025-12-09 12:45:16 +0300

    Update dependencies and author email in setup.py
    Updated Django version requirement and adjusted author email format. Added support for Python 3.14.

--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ setup(
     name='django-computedfields',
     packages=find_packages(exclude=['example']),
     include_package_data=True,
+    python_requires='>=3.10',
     install_requires=[
-        'Django>=4.2,<6.0',
+        'Django>=5.0,<7.0',
         'typing_extensions>=4.1',
         'django-fast-update'
     ],
@@ -24,7 +25,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='netzkolchose',
-    author_email='j.breitbart@netzkolchose.de',
+    author_email='j. breitbart@netzkolchose.de',
     url='https://github.com/netzkolchose/django-computedfields',
     download_url=f'https://github.com/netzkolchose/django-computedfields/archive/{get_version()}.tar.gz',
     keywords=['django', 'denormalization', 'method', 'decorator',
@@ -36,15 +37,15 @@ setup(
         'Topic :: Database :: Front-Ends',
         'Topic :: Software Development :: Libraries',
         'Framework :: Django',
-        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
         'Framework :: Django :: 5.1',
         'Framework :: Django :: 5.2',
+        'Framework :: Django :: 6.0',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        'Programming Language ::  Python :: 3.11',
         'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13'
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14'
     ],
 )
